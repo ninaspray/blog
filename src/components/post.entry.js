0690ff06e55381d095393bff5acc3d9fed7adae8
ninaspray <ninaspray@gmail.com>
2021-04-09 21:28:08 +0100

    Added Like button using use state to react app

--- src/components/post.entry.js
+++ src/components/post.entry.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //types
 import { PostType } from '../types/post.type';
 import { UserType } from '../types/user.type';
 
 const PostEntry = ({ post, user }) => {
-  const { title, body } = post || {};
-  const { name } = user || {};
+  const { title, body } = post;
+  const { name } = user;
+
+  const [likeCounter, setLikeCounter] = useState(0);
+
+  const updateCounter = () => {
+      setLikeCounter(previousCounter => previousCounter + 1 );
+  };
+
   return (
    <article>
     <h2>{title}</h2>
     <h3>by {name}</h3>
     <p>{body}</p>
-    </article>
+    <div>
+    <span>{likeCounter}</span>
+<button type="button" onClick={() => updateCounter()}>
++
+</button>
+</div>
+</article>
     );
   };
 
