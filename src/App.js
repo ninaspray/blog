377edeedc238603d7f9f753585ebb1367242b354
ninaspray <ninaspray@gmail.com>
2021-04-13 21:08:51 +0100

    installed axios promise based HTTP client for the browser

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 // components
 import PostList from './components/post.list';
 
 // mock data
-import posts from './data/posts.json';
-import users from './data/users.json';
+import mockPosts from './data/posts.json';
+import mockUsers from './data/users.json';
 
 const App = () => {
     const [theme, setTheme] = useState('light');
+    const [posts, setPosts] = useState([]);
+    const [users, setUsers] = useState([]);
+
+
     const themeConfig = {
         light: { background: '#eee', color: '#333' },
         dark: { background: '#333', color: '#eee'  },
@@ -18,6 +22,11 @@ const App = () => {
     const updateTheme = selectedTheme => {
         setTheme(selectedTheme);
     };
+
+    useEffect(() => {
+        setPosts(mockPosts);
+        setUsers(mockUsers);
+    }, []);
 
     return (
         <div style={themeConfig[theme]}>
@@ -32,7 +41,11 @@ const App = () => {
                     </button>
                 </div>
             </header>
+            {posts.lenght > 0 && users.length > 0 ? (
             <PostList posts={posts} users={users} />
+            ) : (
+             <div>Loading...</div>   
+            )}
         </div>
     );
 };
