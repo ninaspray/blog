5afcbaeaeabea9e2d5351279186ac6b40628bcc5
ninaspray <ninaspray@gmail.com>
2021-04-16 17:41:01 +0100

    Added Tests relating to comment list component, using test information

---
+++ src/tests/comment-list.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {render, screen} from "@testing-library/react";
+import '@testing-library/jest-dom';
+import '../components/comment-list';
+import CommentList from '../components/comment-list';
+
+//Adding Test information for user comments
+const comments = [
+    {
+        body: "Test comment body 1",
+        username: "Test comment username 1",
+        uuid: `Test id 123`,
+      },
+      {
+        body: "Test comment body 2",
+        username: "Test comment username 2",
+        uuid: "Test id 124",
+      },
+      {
+        body: "Test comment body 3",
+        username: "Test comment username 3",
+        uuid: "Test id 125",
+      }
+    ];
+
+describe("comment list", () => {
+    it("render expected number of comments", () => {
+    const { getAllByTestId } = render(
+    <CommentList comments={comments} />
+);
+  expect(getAllByTestId("comment")).toHaveLength(3);
+});
+}); 
