18af9c942d549c3892251a51b5beab5e082ab7c8
ninaspray <ninaspray@gmail.com>
2021-04-16 18:05:39 +0100

    Added a test to test the functionality of adding likes

--- src/tests/like-counter.test.js
+++ src/tests/like-counter.test.js
@@ -4,6 +4,13 @@ import '@testing-library/jest-dom'
 import LikeCounter from "../components/like-counter";
 
 describe('LikeCounter', () => {
+    const updateCounter = jest.fn();
+    const counts = [0, 1, 2, 10];
+    it.each(counts)('display %s likes when expected', (count) => {
+      render(<LikeCounter likeCounter={count} handleClick={updateCounter} />);
+      expect(screen.getByText(count)).toBeInTheDocument();
+    });
+
     it('calls updateCounter prop when clicked', () => {
       const updateCounter = jest.fn();
       const likeCounter = 0;
