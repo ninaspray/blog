7fe3a4d055c07e0aa0651626cd7f6ace3374bbd6
ninaspray <ninaspray@gmail.com>
2021-05-01 19:00:18 +0100

    Added a global style for button for consitenct on my webpage

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -15,3 +15,26 @@ export const PageContainer = Styled.div`
     padding-right: 12px;
     box-sizing: border-box;
 `;
+
+export const StyledHeader = Styled.header`
+    font-size: 28px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 20px;
+    padding-bottom: 40px;
+    .header_title {
+        margin-bottom: 20px;
+    }
+    .header_button-wrap {
+        display: flex;
+        flex-grow: 1
+    }
+
+`;
+
+export const StyleButton = Styled.button`
+    padding: 10px 20px;
+    font-size: 1rem;
+    font-weight: 700;
+    `;
