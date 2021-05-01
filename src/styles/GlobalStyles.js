84f720a8da9e676ca98e0b76700d1ef5b447c9cd
ninaspray <ninaspray@gmail.com>
2021-05-01 18:23:28 +0100

    Added global styles file

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,16 @@
+import Styled from 'styled-components';
+
+export const PageWrapper = Styled.div`
+
+    height: 100vh;
+    `;
+
+export const PageContainer = Styled.div`
+    display: block;
+    max-width: 1024px;
+    width: 100%;
+    margin-left: auto;
+    margin-right: auto;
+    padding-left: 12px;
+    padding-right: 12px;
+`;
